f4bdaddc500fd132976d85980dec014a7935569e
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-07-10 23:30:26 -0400

    Updates

---
+++ precache-manifest.37f47af222c966b59ad26f66c705cb6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faecdfd8c1a8beee2389237ee76b8d09",
+    "url": "/diversion_con_banderas/index.html"
+  },
+  {
+    "revision": "d38fa7f63bbaec94dc2c",
+    "url": "/diversion_con_banderas/static/css/main.713c67f1.chunk.css"
+  },
+  {
+    "revision": "9e680710d78a14318369",
+    "url": "/diversion_con_banderas/static/js/2.e0da8f4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/diversion_con_banderas/static/js/2.e0da8f4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d38fa7f63bbaec94dc2c",
+    "url": "/diversion_con_banderas/static/js/main.1ecb031b.chunk.js"
+  },
+  {
+    "revision": "82bbc8d30596ae0aa27e",
+    "url": "/diversion_con_banderas/static/js/runtime-main.b49d0ba1.js"
+  }
+]);
